ce7a5aa45626efcf8ee66fc43106e89901a04623
mrken <849946945@qq.com>
2013-10-15 21:22:54 +0800

    commited,ken!

---
+++ HelloWindow/HelloWindow/HelloFrm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace HelloWindow
+{
+    partial class HelloFrm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb1Show = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(183, 36);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "确定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "姓名";
+            // 
+            // lb1Show
+            // 
+            this.lb1Show.AutoSize = true;
+            this.lb1Show.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lb1Show.Location = new System.Drawing.Point(25, 103);
+            this.lb1Show.Name = "lb1Show";
+            this.lb1Show.Size = new System.Drawing.Size(0, 14);
+            this.lb1Show.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(24, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(73, 37);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 4;
+            // 
+            // HelloFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 223);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lb1Show);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Name = "HelloFrm";
+            this.Text = "可输入的窗体";
+            this.Load += new System.EventHandler(this.HelloFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb1Show;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ HelloWindow/HelloWindow/HelloFrm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloWindow
+{
+    public partial class HelloFrm : Form
+    {
+        public HelloFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloFrm_Load(object sender, EventArgs e)
+        {
+            this.Text = "可输入的窗体";
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string result;
+            result = txtName.Text+"同学，";
+            result += "已完成作业";
+            lb1Show.Text = result;
+            label3.Text = "肿么有介摸二逼的交作业方式?!";
+        }
+    }
+}
